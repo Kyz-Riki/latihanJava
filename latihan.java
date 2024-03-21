9975a25a3e95314465b4b9411ed7437af998affe
Kyz-Riki <kiritoizumi07@gmail.com>
2024-03-21 17:05:18 +0700

    latihanJava

---
+++ latihan.java
@@ -0,0 +1,12 @@
+public class latihan {
+    public static void main(String[] args) {
+         int jariJari;
+         double keliling;
+         final double pi = 3.14;
+
+         jariJari = 30;
+         keliling = 2*pi*jariJari;
+         System.out.println("Keliling Lingkaran = "+keliling);
+        System.out.println("Project Hello World!");
+    }
+}
---
+++ latihanVariable.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanp01;
+
+/**
+ *
+ * @author Asus
+ */
+public class latihanVariable {
+    public static void main(String[] args) {
+         int jariJari;
+         double keliling;
+         final double pi = 3.14;
+
+         jariJari = 30;
+         keliling = 2*pi*jariJari;
+         System.out.println("Keliling Lingkaran = "+keliling);
+        System.out.println("===============================");
+    }
+}
